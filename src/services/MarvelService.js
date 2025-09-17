0ca10db17e2eeab773bcdf7499d2319830857b28
Viktar Vasileuski <victor.vasilev1991@gmail.com>
2025-09-17 09:57:43 +0300

    added styles

---
+++ src/services/MarvelService.js
@@ -0,0 +1,35 @@
+class MarvelService {
+    _baseUrl = "https://marvel-server-zeta.vercel.app/"
+    _apiKey = "d4eecb0c66dedbfae4eab45d312fc1df";
+
+    getResource = async (path) => {
+        const url = `${this._baseUrl}${path}?apikey=${this._apiKey}`;
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Failed to fetch resource ${url}. Status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource('characters');
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`characters/${id}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    }
+}
+
+export default MarvelService;
